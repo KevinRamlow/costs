179e11b70b222c423d8b5a2a86216ad77a0a916e
KevinRamlow <kevinandraderamlow@gmail.com>
2023-12-05 13:44:02 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Home from './components/pages/Home';
-import Company from './components/pages/Company';
-import Contact from './components/pages/Contact';
-import Projects from './components/pages/Projects';
+import Home from './components/pages/home/Home';
 import NavBar from './components/layout/navBar/NavBar';
 import NewProject from './components/pages/NewProject';
 import Footer from './components/layout/footer/Footer';
+import Company from './components/pages/company/Company';
+import Contact from './components/pages/contact/Contact';
+import Projects from './components/pages/projects/Projects';
 import Container from './components/layout/container/Container';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
---
+++ src/components/layout/button/LinkButton.js
@@ -0,0 +1,11 @@
+import styles from './LinkButton.module.css'
+
+import { Link } from 'react-router-dom'
+
+export default function LinkButton({ to, text }) {
+  return (
+    <Link className={styles.btn} to={to}> 
+      {text}
+    </Link>
+  )
+}
