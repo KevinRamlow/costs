229b9107c3d0cccbf0115416766aba457d8d70ab
KevinRamlow <kevinandraderamlow@gmail.com>
2023-12-05 14:01:44 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Home from './components/pages/home/Home';
 import NavBar from './components/layout/navBar/NavBar';
-import NewProject from './components/pages/NewProject';
 import Footer from './components/layout/footer/Footer';
 import Company from './components/pages/company/Company';
 import Contact from './components/pages/contact/Contact';
 import Projects from './components/pages/projects/Projects';
 import Container from './components/layout/container/Container';
+import NewProject from './components/pages/newProject/NewProject';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
---
+++ src/components/pages/newProject/NewProject.js
@@ -0,0 +1,12 @@
+import styles from './NewProject.module.css'
+import ProjectForm from '../../project/ProjectForm'
+
+export default function NewProject() {
+  return (
+    <section className={styles.newproject_container}>
+      <h1>Criar projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm />
+    </section>
+  )
+}
